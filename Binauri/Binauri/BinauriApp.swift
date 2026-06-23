0b00e722a23e548e4f6065efc950978ab1970922
Aqib Mehmood <aqibmehmood@Aqibs-MacBook-Pro.local>
2026-06-23 10:01:13 +0500

    Initial iOS project setup with SwiftUI

---
+++ Binauri/Binauri/BinauriApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinauriApp.swift
+//  Binauri
+//
+//  Created by Aqib Mehmood on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BinauriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
